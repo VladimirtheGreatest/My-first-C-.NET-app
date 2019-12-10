322cb709917494c6144b11eeb17664b06e56e9e9
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-10 17:27:19 +0000

    testing git

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -10,6 +10,12 @@ namespace myApp
             Console.WriteLine("The current time is " + DateTime.Now);
 
                   int[] array1 = new int[] { 1, 3, 5, 7, 9 };
+                  array1[1] = 900;
+            int[] array2 = new int[3];
+            array2[0] = 2;
+            array2[1] = 4;
+            array2[2] = 6;
+            Console.WriteLine(array2[1]);
 
             for (int i = 0; i < array1.Length; i++)
             {
