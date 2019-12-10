207bb43dea639f125a6fbe5b167d6b823c0e1f69
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-10 17:00:36 +0000

    testing git

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -8,6 +8,20 @@ namespace myApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("The current time is " + DateTime.Now);
+
+                  int[] array1 = new int[] { 1, 3, 5, 7, 9 };
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] > 5)
+                {
+                    Console.WriteLine(array1[i]);
+                }
+            }
+                        
+           
+
+
         }
     }
 }
