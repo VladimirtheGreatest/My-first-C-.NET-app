a9178f5f8608b9d049f3a32d0e611d454da09777
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-11 21:39:15 +0000

    Building a simple calculator, c# practice.

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -6,27 +6,40 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Better calculator 1.0");
             Console.WriteLine("The current time is " + DateTime.Now);
 
-                  int[] array1 = new int[] { 1, 3, 5, 7, 9 };
-                  array1[1] = 900;
-            int[] array2 = new int[3];
-            array2[0] = 2;
-            array2[1] = 4;
-            array2[2] = 6;
-            Console.WriteLine(array2[1]);
+             Console.Write("Enter your number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
 
-            for (int i = 0; i < array1.Length; i++)
+            Console.Write("Enter the operator: ");
+            string Operator = Console.ReadLine();
+
+            Console.Write("Enter your second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double Result = 0;
+
+            if(Operator == "+")
             {
-                if (array1[i] > 5)
-                {
-                    Console.WriteLine(array1[i]);
-                }
+                Result = number1 + number2;
+                Console.WriteLine(Result);
+            } else if (Operator == "-")
+            {
+                Result = number1 - number2;
+                Console.WriteLine(Result);
+            } else if (Operator == "*")
+            {
+                Result = number1 * number2;
+                Console.WriteLine(Result);
+            } else if (Operator == "*")
+            {
+                Result = number1 / number2;
+                Console.WriteLine(Result);
+            } else
+            {
+                Console.WriteLine("Invalid operator");
             }
-                        
-           
-
 
         }
     }
